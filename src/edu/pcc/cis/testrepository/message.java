da2eb3e811c7ea0b5793f6c33af3784f2785eb5d
Hannah.Doty <hannah.doty@pcc.edu>
2023-01-23 16:43:07 -0800

    Created the findValue method

--- src/edu/pcc/cis/testrepository/message.java
+++ src/edu/pcc/cis/testrepository/message.java
@@ -3,9 +3,15 @@ package edu.pcc.cis.testrepository;
 public class message {
     public message() {
         printMessage();
+        findValue(7, 57);
     }
     public void printMessage() {
         System.out.println("Print out a message");
+    }
+
+    public int findValue(int a, int b) {
+        int sum = a + b;
+        return sum;
     }
 }
 
