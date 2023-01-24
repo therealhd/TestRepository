8c3940ed12d8e1397f677bf9baae17c52d87df74
Hannah.Doty <hannah.doty@pcc.edu>
2023-01-23 16:40:54 -0800

    Created the Main class, added a printMessage method to the Message class

---
+++ src/edu/pcc/cis/testrepository/main.java
@@ -0,0 +1,7 @@
+package edu.pcc.cis.testrepository;
+
+public class main {
+    public static void main(String[] args) {
+        new message();
+    }
+}
--- src/edu/pcc/cis/testrepository/message.java
+++ src/edu/pcc/cis/testrepository/message.java
@@ -1,4 +1,13 @@
 package edu.pcc.cis.testrepository;
 
 public class message {
+    public message() {
+        printMessage();
+    }
+    public void printMessage() {
+        System.out.println("Print out a message");
+    }
 }
+
+
+
